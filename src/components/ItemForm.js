4be28636d95a52295c4f406ae7ff32dfe790450f
Ronnie Wanjala <ranjala90@gmail.com>
2024-08-12 15:25:36 +0300

    add fetch

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -12,7 +12,15 @@ function ItemForm() {
       category: category,
       isInCart: false,
     };
-    console.log(itemData);
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => console.log(newItem));
   }
 
   return (
